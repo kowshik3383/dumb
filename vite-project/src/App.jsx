7c77742efd18dc0863c10ce04389dafe72683506
kowshik <kowshikvalpireddy@gmail.com>
2024-03-20 17:34:49 +0530

    dfsdf

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,20 +1,46 @@
-import React from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import About from "./About";
-import Kow from "./Kow";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignUp from "./SignUp";
+import SignIn from "./SignIn";
+import Hero from "./components/Hero";
+import FeatureSection from "./FeatureSection";
+import About from "./components/About";
+import Pricing from "./components/Pricing";
+import Contact from "./components/Contact";
+import Loading from "./components/Loading";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
+
   return (
-    <BrowserRouter>
-      <div>
-        <Link to="/k">Go to About</Link>
-        <Link to="/a">Go to Kow</Link>
-      </div>
-      <Routes>
-        <Route path="/k" element={<About />} />
-        <Route path="/a" element={<Kow />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      {loading ? (
+        <Loading />
+      ) : (
+        <BrowserRouter>
+          <Routes>
+            <Route path="/sign-up" element={<SignUp />} />
+            <Route path="/sign-in" element={<SignIn />} />
+            <Route path="/" element={
+              <>
+                <Hero />
+                <FeatureSection />
+                <About />
+                <Pricing />
+                <Contact />
+              </>
+            } />
+            
+          </Routes>
+        </BrowserRouter>
+      )}
+    </>
   );
 }
 
